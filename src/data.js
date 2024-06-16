827aa5d49cdf0b9b78069938ba4178de16024136
balananujith <balananujith@gmail.com>
2024-06-16 14:52:32 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,63 @@
+export const projects = [
+  {
+    title: "Youtube Video Summarizer",
+    subtitle: "Python",
+    description:
+      "Our Tool Utilizes The YouTube Transcript API To Extract The Transcript Text From A Given YouTube Video URL. Then, It Leverages Google Generative AI To Generate Detailed Notes Based On The Extracted Transcript. The Final Output Is A Succinct Summary Of The Video Content, Making It Easier For You To Understand The Main Points Covered In The Video. By clicking over this you will get to my Github Repo!!",
+    image: "./YT1.jpg",
+    link: "https://github.com/balananujith/yt_summarizer5.git",
+  },
+  {
+    title: "Movie Recommendation System",
+    subtitle: "Machine Learning",
+    description:
+      "A Flask-Based Web App Utilizing Python And CSV Files To Deliver Personalized Movie Recommendations. Powered By Machine Learning, It Predicts User Preferences Based On Movie Attributes Like Cast, Crew, And Overview, Aiding In Overcoming Information Overload By Suggesting Tailored Content. By clicking on this, you will get to my Github Repo !!",
+    image: "./1.jpeg",
+    link: "https://github.com/balananujith/movie_recommender_app.git",
+  },
+  {
+    title: "Snake-N-Apple",
+    subtitle: "Web Application",
+    description:
+      "This Project Is A Classic Implementation Of The Snake And Apple Game, Developed Using Python For The Backend Logic And HTML, CSS And Javascript For The Frontend User Interface. The Game Utilizes Javascript For Handling Various Functionalities And Interactions. By clicking on this, you will get to my Github Repo!!",
+    image: "./snake1.jpg",
+    link: "https://github.com/balananujith/snake-n-apple.git",
+  },
+  {
+    title: "Flight Price Prediction",
+    subtitle: "Machine Learning",
+    description:
+      "This Project Aims To Create A Python Web App For Predicting Flight Fares Based On User-Provided Details Like Departure Date, City, And Airline Carrier, Tackling The Complexity Of Airline Pricing Dynamics. By clicking on this, you will get to my corresponding Github Repo!!",
+    image: "./flight1.jpg",
+    link: "https://github.com/balananujith/flight-price-prediction2.git",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Anujith brought fresh ideas and a lot of enthusiasm to our project. Despite being new to the field, his ability to grasp our requirements quickly and deliver a functional and appealing website was impressive. His dedication and hard work truly made a difference.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "John Doe",
+    company: "Startup Founder",
+  },
+  {
+    quote:
+      "Working with Anujith was a great experience. He is eager to learn and highly adaptable. He developed our mobile app from scratch and it turned out fantastic. His attention to detail and willingness to go the extra mile are commendable.",
+    image: "https://randomuser.me/api/portraits/men/84.jpg",
+    name: "Jane Smith",
+    company: "Project Lead at InnovateX",
+  },
+];
+
+export const skills = [
+  "JAVASCRIPT",
+  "REACT",
+  "HTML",
+  "BOOTSTRAP",
+  "CSS",
+  "C++",
+  "C",
+  "FLUTTER",
+  "JAVA",
+];
